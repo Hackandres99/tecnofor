b72125e587a5afdf33ec45c88180de3a87b1b5b8
Andrés Saavedra <andres19saavedra9castro9@gmail.com>
2022-08-24 10:30:58 -0500

    Changing footer medias

--- layouts/base/footer.php
+++ layouts/base/footer.php
@@ -3,7 +3,9 @@
     <?php include 'header/logo.php' ?>
     
     <div class="footer_medias">
+        <label for="">¿Tienes alguna pregunta?</label>
         <a href="?p=contacto"><i class="fas fa-envelope footer_media_icon"></i></a>
+        <label for="">Mándanos un mensaje</label>
     </div>
 
     <p class="footer_copy">
