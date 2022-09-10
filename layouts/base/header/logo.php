0ce18897ad07b0e3a7cfb2d6a1382db8cf45d6a2
Andrés Saavedra <andres19saavedra9castro9@gmail.com>
2022-09-10 14:19:20 -0500

    Modifiying logo and session colors

--- layouts/base/header/logo.php
+++ layouts/base/header/logo.php
@@ -1,11 +1,11 @@
 <h1 class="logo">
     <a class="logo_img_link" href="?p=inicio">
         <img class="logo_img" src="/img/logo.png" alt="logo">
-        <label class="logo_name">
-            TECNOFOR
-        </label>   
+        <span class="logo_name">
+            TECNO<label class="logo_lastname">FOR</label> 
+        </span>   
     </a>
    <a class="logo_sentence_link"href="?p=inicio">
-        AFIANZA TU APRENDIZAJE
+        FORTALECE TU APRENDIZAJE CON LA TECNOLOGÍA.
     </a>
 </h1>
